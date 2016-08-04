846ed8e4a10c614a6f24623257b6ab26c0af8077
Michael Furlong <furthing45@Michaels-MacBook-Pro-4.local>
2016-08-04 16:20:12 -0700

    fix rotational squares, rearrange face image, being animation controller

---
+++ Temp/StagingArea/Data/il2cppOutput/AssemblyU2DUnityScript_transform1052666732MethodDeclarations.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "il2cpp-config.h"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+#include <stdint.h>
+#include <assert.h>
+#include <exception>
+
+// transform
+struct transform_t1052666732;
+
+#include "codegen/il2cpp-codegen.h"
+
+// System.Void transform::.ctor()
+extern "C"  void transform__ctor_m3169439658 (transform_t1052666732 * __this, const MethodInfo* method) IL2CPP_METHOD_ATTR;
+// System.Void transform::Start()
+extern "C"  void transform_Start_m2116577450 (transform_t1052666732 * __this, const MethodInfo* method) IL2CPP_METHOD_ATTR;
+// System.Void transform::Update()
+extern "C"  void transform_Update_m1195243683 (transform_t1052666732 * __this, const MethodInfo* method) IL2CPP_METHOD_ATTR;
+// System.Void transform::Main()
+extern "C"  void transform_Main_m294829011 (transform_t1052666732 * __this, const MethodInfo* method) IL2CPP_METHOD_ATTR;
---
+++ Temp/StagingArea/Data/il2cppOutput/Il2CppDelegateWrappersManagedToNativeTable.cpp
@@ -0,0 +1,44 @@
+﻿#include "il2cpp-config.h"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+
+#include "class-internals.h"
+#include "codegen/il2cpp-codegen.h"
+
+extern "C" void DelegatePInvokeWrapper_AppDomainInitializer_t691583313 ();
+extern "C" void DelegatePInvokeWrapper_Swapper_t4166107989 ();
+extern "C" void DelegatePInvokeWrapper_ReadDelegate_t1428404869 ();
+extern "C" void DelegatePInvokeWrapper_WriteDelegate_t2583486074 ();
+extern "C" void DelegatePInvokeWrapper_CrossContextDelegate_t651537830 ();
+extern "C" void DelegatePInvokeWrapper_CallbackHandler_t1474775431 ();
+extern "C" void DelegatePInvokeWrapper_ThreadStart_t124146534 ();
+extern "C" void DelegatePInvokeWrapper_CostDelegate_t1292950321 ();
+extern "C" void DelegatePInvokeWrapper_LogCallback_t2984951347 ();
+extern "C" void DelegatePInvokeWrapper_PCMReaderCallback_t83861602 ();
+extern "C" void DelegatePInvokeWrapper_PCMSetPositionCallback_t4244274966 ();
+extern "C" void DelegatePInvokeWrapper_AudioConfigurationChangeHandler_t1377657005 ();
+extern "C" void DelegatePInvokeWrapper_StateChanged_t2578300556 ();
+extern "C" void DelegatePInvokeWrapper_DisplaysUpdatedDelegate_t581305515 ();
+extern const Il2CppMethodPointer g_DelegateWrappersManagedToNative[14] = 
+{
+	DelegatePInvokeWrapper_AppDomainInitializer_t691583313,
+	DelegatePInvokeWrapper_Swapper_t4166107989,
+	DelegatePInvokeWrapper_ReadDelegate_t1428404869,
+	DelegatePInvokeWrapper_WriteDelegate_t2583486074,
+	DelegatePInvokeWrapper_CrossContextDelegate_t651537830,
+	DelegatePInvokeWrapper_CallbackHandler_t1474775431,
+	DelegatePInvokeWrapper_ThreadStart_t124146534,
+	DelegatePInvokeWrapper_CostDelegate_t1292950321,
+	DelegatePInvokeWrapper_LogCallback_t2984951347,
+	DelegatePInvokeWrapper_PCMReaderCallback_t83861602,
+	DelegatePInvokeWrapper_PCMSetPositionCallback_t4244274966,
+	DelegatePInvokeWrapper_AudioConfigurationChangeHandler_t1377657005,
+	DelegatePInvokeWrapper_StateChanged_t2578300556,
+	DelegatePInvokeWrapper_DisplaysUpdatedDelegate_t581305515,
+};
